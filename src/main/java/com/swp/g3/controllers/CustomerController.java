d08e358c15485c525ab52640adfe0895860e2603
CuongLD3001 <106051110+CuongLD3001@users.noreply.github.com>
2023-02-17 09:34:23 +0700

    Update CustomerController.java

--- src/main/java/com/swp/g3/controllers/CustomerController.java
+++ src/main/java/com/swp/g3/controllers/CustomerController.java
@@ -1,8 +1,36 @@
 package com.swp.g3.controllers;
 
-public class CustomerController {
-}
+import com.swp.g3.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+public class CustomerController {
+    @Autowired
+    UserService userService;
+    @Autowired
+    EmailService emailService;
+    @PostMapping(value = "/api/user/register")
+    public boolean register(@Valid @RequestBody Customer customer){
+        try{
+            userService.save(customer);
+            emailService.sendEmail(customer.getGmail(), customer.getUsername(), customer.getName());
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    @GetMapping(value = "/api/user/username/duplicate/{username}")
+    public boolean checkExistedUsername(@PathVariable String username){
+        return userService.findOneByUsername(username) != null;
+    }
+
+    @GetMapping(value = "/api/user/gmail/duplicate/{gmail}")
+    public boolean checkExistedEmail(@PathVariable String gmail){
+        return userService.findOneByGmail(gmail) != null;
+    }
+}
 
 
 
