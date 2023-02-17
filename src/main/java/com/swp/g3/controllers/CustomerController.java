85e84bd4e49d1e545f8d0f9db20ada535b17a00c
CuongLD3001 <106051110+CuongLD3001@users.noreply.github.com>
2023-02-17 09:51:04 +0700

    Update CustomerController.java

--- src/main/java/com/swp/g3/controllers/CustomerController.java
+++ src/main/java/com/swp/g3/controllers/CustomerController.java
@@ -1,35 +1,41 @@
 package com.swp.g3.controllers;
 
 import com.swp.g3.entity.Customer;
+import com.swp.g3.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerController {
     @Autowired
-    UserService userService;
-    @Autowired
-    EmailService emailService;
-    @PostMapping(value = "/api/user/register")
-    public boolean register(@Valid @RequestBody Customer customer){
-        try{
-            userService.save(customer);
-            emailService.sendEmail(customer.getGmail(), customer.getUsername(), customer.getName());
-            return true;
-        }catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }
-    }
-    @GetMapping(value = "/api/user/username/duplicate/{username}")
-    public boolean checkExistedUsername(@PathVariable String username){
-        return userService.findOneByUsername(username) != null;
+    CustomerService customerService;
+
+//    @PostMapping(value = "/api/user/register")
+//    public boolean register(@Valid @RequestBody Customer customer){
+//        try{
+//            userService.save(customer);
+//            emailService.sendEmail(customer.getGmail(), customer.getUsername(), customer.getName());
+//            return true;
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+//    @GetMapping(value = "/api/user/username/duplicate/{username}")
+//    public boolean checkExistedUsername(@PathVariable String username){
+//        return userService.findOneByUsername(username) != null;
+//    }
+//
+//    @GetMapping(value = "/api/user/gmail/duplicate/{gmail}")
+//    public boolean checkExistedEmail(@PathVariable String gmail){
+//        return userService.findOneByGmail(gmail) != null;
+//    }
+
+    @GetMapping(value = "api/customer/detail/{id}")
+    public boolean checkExistedId(@PathVariable Integer id){
+        return customerService.
     }
 
-    @GetMapping(value = "/api/user/gmail/duplicate/{gmail}")
-    public boolean checkExistedEmail(@PathVariable String gmail){
-        return userService.findOneByGmail(gmail) != null;
-    }
 }
 
 
