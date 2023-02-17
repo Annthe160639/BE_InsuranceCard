3f4e07d4c22fb353f3f2c4b44f470e97f3e7431a
CuongLD3001 <106051110+CuongLD3001@users.noreply.github.com>
2023-02-17 09:36:16 +0700

    Create CustomerService.java

---
+++ src/main/java/com/swp/g3/service/CustomerService.java
@@ -0,0 +1,31 @@
+package com.swp.g3.service;
+
+import com.swp.g3.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+
+    public boolean save(Customer newCustomer){
+        try{
+            customerRepository.save(newCustomer);
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    public Customer findOneByUsername(String username){
+        Customer c = customerRepository.findOneByUsername(username);
+        return c;
+    }
+    public Customer findOneByGmail(String gmail){
+        Customer c = customerRepository.findOneByGmail(gmail);
+        return c;
+    }
+
+}
+
