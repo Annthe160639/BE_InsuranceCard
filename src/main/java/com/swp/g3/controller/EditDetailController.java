415bd2e8dfc83b8f23291b6e0acdc6e14d47a922
CuongLD3001 <106051110+CuongLD3001@users.noreply.github.com>
2023-02-26 23:37:42 +0700

    Update EditDetailController.java

--- src/main/java/com/swp/g3/controller/EditDetailController.java
+++ src/main/java/com/swp/g3/controller/EditDetailController.java
@@ -2,30 +2,33 @@ package com.swp.g3.controller;
 
 import com.swp.g3.entity.Customer;
 import com.swp.g3.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EditDetailController {
+    @Autowired
     CustomerRepository customerRepository;
 
 
-    @RequestMapping(value = "welcome/edit/{username}", method = RequestMethod.GET)
+    @RequestMapping(value = "/welcome/edit/{username}", method = RequestMethod.GET)
     public String getEditUserData(@PathVariable("username") String username, Model model, Customer accountForm) {
 
         model.addAttribute("username", username);
         return "editProfile";
     }
 
-    @RequestMapping(value = "editProfile", method = RequestMethod.GET)
+    @RequestMapping(value = "/editProfile", method = RequestMethod.GET)
     public String showEditProfilePage(Model model, Customer customer) {
         model.addAttribute("username", customer.getUsername());
         model.addAttribute("password", customer.getPassword());
         return "editProfile";
     }
 
-    @RequestMapping(value = "editProfile", method = RequestMethod.POST)
-    public String sendEditProfilePage(@ModelAttribute("accountForm") Customer customer) {
+    @RequestMapping(value = "/editProfile", method = RequestMethod.POST)
+    public String sendEditProfilePage(@RequestBody Customer customer) {
+
         Customer accountInstance = customerRepository.findOneByUsername(customer.getUsername());
 
         accountInstance.setName(customer.getName());
@@ -36,8 +39,7 @@ public class EditDetailController {
         accountInstance.setManagerId(customer.getManagerId());
 
 
-        customerRepository.save(customer);
-
+        customerRepository.save(accountInstance);
         return "editProfile";
     }
 }
