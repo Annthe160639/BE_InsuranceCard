a5eb902a5ea9b64751797c0e8affdd6a88a42da5
CuongLD3001 <106051110+CuongLD3001@users.noreply.github.com>
2023-03-05 23:26:47 +0700

    Update CustomerRepository.java

--- src/main/java/com/swp/g3/repository/CustomerRepository.java
+++ src/main/java/com/swp/g3/repository/CustomerRepository.java
@@ -7,12 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     public Customer findOneByUsernameAndPassword(String username, String password);
     public Customer save(Customer newCustomer);
     public Customer findOneByUsername(String username);
     public Customer findOneByGmail(String gmail);
+    public List<Customer> findAll();
+
+    public Customer findByID(int id);
     @Query("SELECT c FROM Customer c")
     Page<Customer> findCustomers(Pageable pageable);
 }
