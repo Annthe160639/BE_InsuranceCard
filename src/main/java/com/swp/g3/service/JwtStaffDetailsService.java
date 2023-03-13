048edb2de779bc3b409c659babdecdd664880be0
Annthe160639 <annthe160639@fpt.edu.vn>
2023-03-13 11:44:55 +0700

    merge

---
+++ src/main/java/com/swp/g3/service/JwtStaffDetailsService.java
@@ -0,0 +1,39 @@
+package com.swp.g3.service;
+
+import com.swp.g3.entity.Customer;
+import com.swp.g3.entity.Staff;
+import com.swp.g3.repository.CustomerRepository;
+import com.swp.g3.repository.StaffRepository;
+import com.swp.g3.util.Crypto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class JwtStaffDetailsService implements UserDetailsService {
+    @Autowired
+    StaffRepository staffRepository;
+    @Autowired
+    public Crypto crypto;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Staff c = staffRepository.findOneByUsername(username);
+        if (c != null) {
+            try {
+                return new User(c.getUsername(), new BCryptPasswordEncoder().encode(crypto.decrypt(c.getPassword())),
+                        Collections.singleton(new SimpleGrantedAuthority(c.getRole())));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+}
